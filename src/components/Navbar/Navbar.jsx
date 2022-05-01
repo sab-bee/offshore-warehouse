0836110b3d3ca81332778cb81ac21885539dc13c
sabbir-dcy <sabbirahmd.bracu@gmail.com>
2022-05-01 07:07:04 +0600

    responsive nabbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import { signOut } from 'firebase/auth'
-import React from 'react'
+import React, { useState } from 'react'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { Link, useNavigate } from 'react-router-dom'
 import { auth } from '../../firebase/firebase.init'
@@ -9,39 +9,74 @@ import { motion } from 'framer-motion'
 const Navbar = () => {
   const [user] = useAuthState(auth)
   const navigate = useNavigate()
+  const [expand, setExpand] = useState(false)
 
   return (
-    <motion.div
-      className='shadow-lg bg-accent-brown'
-      initial={{ y: -100, opacity: 0 }}
-      animate={{ y: 0, opacity: 1 }}
-      transition={{ duration: 1, ease: 'easeOut', delay: 0.5 }}
-    >
-      <div className='flex w-4/5 h-20 mx-auto justify-between items-center'>
-        <div className=''>
-          <ActiveLink to='/'>
-            <h2 className='text-xl'>
-              <span>offshore</span>
-              <br />
-              <span>sotckroom</span>
-            </h2>
-          </ActiveLink>
+    <>
+      <motion.div
+        className='shadow-lg bg-accent-brown hidden md:block'
+        initial={{ y: -100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 1, ease: 'easeOut', delay: 0.5 }}
+      >
+        <div className='flex w-4/5 h-20 mx-auto justify-between items-center'>
+          <div className=''>
+            <ActiveLink to='/'>
+              <h2 className='text-xl'>
+                <span>offshore</span>
+                <br />
+                <span>sotckroom</span>
+              </h2>
+            </ActiveLink>
+          </div>
+          <div className='space-x-5'>
+            <ActiveLink to='/'>Home</ActiveLink>
+            {user ? (
+              <>
+                <ActiveLink to='/manageInventories'>Inventory</ActiveLink>
+                <ActiveLink to='/addProduct'>Add Liquor</ActiveLink>
+                <ActiveLink to='/myLiquors'>My Liquors</ActiveLink>
+                <button onClick={() => signOut(auth)}>logout</button>
+              </>
+            ) : (
+              <ActiveLink to='/user/login'>Login</ActiveLink>
+            )}
+          </div>
         </div>
-        <div className='space-x-5'>
-          <ActiveLink to='/'>Home</ActiveLink>
-          {user ? (
-            <>
-              <ActiveLink to='/manageInventories'>Inventory</ActiveLink>
-              <ActiveLink to='/addProduct'>Add Liquor</ActiveLink>
-              <ActiveLink to='/myLiquors'>My Liquors</ActiveLink>
-              <button onClick={() => signOut(auth)}>logout</button>
-            </>
-          ) : (
-            <ActiveLink to='/user/login'>Login</ActiveLink>
-          )}
+      </motion.div>
+      <motion.div className='md:hidden bg-accent-brown shadow-lg'>
+        <div className='flex mx-auto min-h-[80px] justify-between items-center px-6'>
+          <div>
+            <ActiveLink to='/'>
+              <h2 className='text-xl'>
+                <span>offshore</span>
+                <br />
+                <span>sotckroom</span>
+              </h2>
+            </ActiveLink>
+          </div>
+          <div>
+            <button onClick={() => setExpand(!expand)}>extend</button>
+          </div>
         </div>
-      </div>
-    </motion.div>
+        {expand && (
+          <div className='flex flex-col items-center'>
+            <ActiveLink to='/'>Home</ActiveLink>
+
+            {user ? (
+              <>
+                <ActiveLink to='/manageInventories'>Inventory</ActiveLink>
+                <ActiveLink to='/addProduct'>Add Liquor</ActiveLink>
+                <ActiveLink to='/myLiquors'>My Liquors</ActiveLink>
+                <button onClick={() => signOut(auth)}>logout</button>
+              </>
+            ) : (
+              <ActiveLink to='/user/login'>Login</ActiveLink>
+            )}
+          </div>
+        )}
+      </motion.div>
+    </>
   )
 }
 
