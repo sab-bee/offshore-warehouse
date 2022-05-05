17eec88d6d3ec2d6772f2e60a645cb1eec8aa1f9
sabbir-dcy <sabbirahmd.bracu@gmail.com>
2022-05-06 05:11:43 +0600

    validation

--- src/components/Liquors/Liquors.jsx
+++ src/components/Liquors/Liquors.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+
 import { useLiquors } from '../../hooks/useLiquors'
 import Liquor from '../Liquor/Liquor'
 import { motion } from 'framer-motion'
@@ -6,28 +6,7 @@ import Spinner from '../Spinner/Spinner'
 
 const Liquors = () => {
   const { liquors, loading } = useLiquors(6)
-  const [tamp, setTamp] = useState([])
-  useEffect(() => {
-    setTamp(liquors)
-  }, [liquors])
 
-  function sort() {
-    let min = 0
-    const arr = [...tamp]
-    for (let i = 0; i < arr.length; i++) {
-      min = i
-      for (let j = i + 1; j < arr.length; j++) {
-        if (arr[j].productPrice < arr[min].productPrice) {
-          min = j
-        }
-      }
-      if (min !== i) {
-        ;[arr[i], arr[min]] = [arr[min], arr[i]]
-      }
-    }
-    setTamp([...arr])
-  }
-  console.log(tamp)
   return loading ? (
     <Spinner></Spinner>
   ) : (
@@ -37,10 +16,7 @@ const Liquors = () => {
       whileInView={{ opacity: 1 }}
       transition={{ duration: 0.6, ease: 'easeOut' }}
     >
-      <button className='bg-primary px-6 py-3 hover:bg-indigo-700 rounded text-white' onClick={sort}>
-        sort by price
-      </button>
-      {tamp.map((liquor, index) => {
+      {liquors.map((liquor, index) => {
         if (index > 2) {
           index -= 1
         }
